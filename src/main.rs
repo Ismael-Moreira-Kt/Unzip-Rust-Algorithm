34d0b99aaa80e6bba8b4d5e8c5d31c615aef9c3c
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-06-14 00:31:06 +0100

    feat: Addition of the user guide.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::fs;
+use std::io;
+
+
+
 fn main() {
-    println!("Hello, world!");
+    std::process::exit(real_main());
+}
+
+
+fn real_main() -> i32 {
+    let args: Vec<_> = std::env::args().collect();
+
+    if args.len() < 2 {
+        println!("Usage: {} <filename>", args[0]);
+        
+        return 1;
+    }
 }
