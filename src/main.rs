b420c5ef75199537fa7d1377d57277a91a1f2f4b
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-06-14 01:33:13 +0100

    feat: Initial setup: Open the file and convert it into a ZipArchive

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-use std::fs;
-use std::io;
+use std::env;
+use std::fs::{ self, File };
+use std::io::{ copy };
+use std::path::Path;
+use zip::read::ZipArchive;
+
+
 
 
 
@@ -8,17 +13,17 @@ fn main() {
 }
 
 
+
 fn real_main() -> i32 {
-    let args: Vec<_> = std::env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
         println!("Usage: {} <filename>", args[0]);
-        
         return 1;
     }
 
-    let filename = std::path::Path::new(&*args[1]);
-    let file = fs::File::open(&filename).unwrap();
+    let filename = Path::new(&args[1]);
+    let file = File::open(filename).expect("Failed to open file");
 
-    
+    let mut archive = ZipArchive::new(file).expect("Failed to create archive");
 }
