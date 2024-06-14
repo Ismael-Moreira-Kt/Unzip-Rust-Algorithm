bcae2c6caa4b59d1c6e7cc3551048c600ba7f31b
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-06-14 01:34:19 +0100

    feat: Extract files and directories

--- src/main.rs
+++ src/main.rs
@@ -26,4 +26,13 @@ fn real_main() -> i32 {
     let file = File::open(filename).expect("Failed to open file");
 
     let mut archive = ZipArchive::new(file).expect("Failed to create archive");
+
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i).expect("Failed to read entry");
+        let outpath = match file.enclosed_name() {
+            Some(path) => path.to_owned(),
+            None => continue,
+        };
+        
+    }
 }
